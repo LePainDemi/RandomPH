c9f3ac69829ce603458b36cd9308203e6c21a805
LePainDemi <75485013+LePainDemi@users.noreply.github.com>
2021-12-08 17:58:16 +0100

    Ajout d'un petit message de lancement dans le cmd

--- src/bot.py
+++ src/bot.py
@@ -1,25 +1,22 @@
 # bot.py
 import os
-
 import discord
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+client = commands.Bot(command_prefix = "!")
 
 @client.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+    print("RandomPH est lancé !")
+   
+@client.command()
+async def video(ctx):
+    
 
 client.run(TOKEN)
 
